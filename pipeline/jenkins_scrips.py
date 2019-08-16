bb33dc23ca234527507b3228e186a2a3011320db
wangcq <wangcq@abb1513.com>
2019-08-16 11:51:53 +0800

    Revert "update"
    This reverts commit 04290f09d32832b4b47080661416b2654d44281e.

--- pipeline/jenkins_scrips.py
+++ pipeline/jenkins_scrips.py
@@ -1,9 +1,9 @@
 # coding=utf-8
 import os, re,time
-#  自动创建Jenkins Job
-#  2019年7月24日
+#自动创建Jenkins Job
+#2019年7月24日
 # Url  服务名
-jenkins_home = '/root/.jenkins/jobs/'
+jenkins_home = '/data/jenkins/jenkins2/jobs/'
 jenkins_user = 'root'
 # jenkins_passwd = '116365fd86d63bf507aba962606a5c8956'  Pre token
 jenkins_passwd = '11811f784531053132519844d047186074'   # Dev Token
@@ -16,32 +16,28 @@ with open(jenkins_conf,'r') as f:
         try:
             t = line.split(',')
             git_url = t[0]
-            server_name = t[1]
+            server_name = 'Pre_'+t[1]
             port = t[2].strip('\n')
-            name='Dev_' + server_name
-            if os.path.exists(jenkins_home+name):
-                print '%s Job Is Exist'%name
-                print
+            if os.path.exists(jenkins_home+server_name):
+                print '%s Job Is Exist'%server_name
                 continue
             # java -jar jenkins-cli.jar -s http://10.1.188.121 copy-job SRC DST
             # Copies a job.
             #  SRC : Name of the job to copy
             #  DST : Name of the new job to be created.
-            new_job = jenkins_home + name
+            new_job = jenkins_home + server_name
             print "GiT:", git_url, "服务名:", server_name, "端口号:", port, "新Job:", new_job
-            print
-            result = os.system('/application/java/bin/java -jar /data/deploy/jenkins-cli.jar -s http://192.168.1.62:7000 -auth %s:%s  copy-job template %s'%(jenkins_user,jenkins_passwd,name) )
+            result = os.system('/application/java/bin/java -jar /data/deploy/jenkins-cli.jar -s http://192.168.1.62:7000 -auth %s:%s  copy-job template %s'%(jenkins_user,jenkins_passwd,server_name) )
            # if os.path.exists(new_job):
-            if result == 0:
+            if result == 0:  
                 os.chdir(new_job)
                 os.system("sed -ie 's#PORT#%s#g;s#SERVERNAME#%s#g;s#GITURL#%s#g' config.xml"%(port,server_name,git_url))
-          #      print "sed -ie 's#PORT#%s#g;s#SERVERNAME#%s#g;s#URL#%s#g' config.xml"%(port,server_name,git_url)
+                print "sed -ie 's#PORT#%s#g;s#SERVERNAME#%s#g;s#URL#%s#g' config.xml"%(port,server_name,git_url)
                 print "Create \033[1;35m Success \033[0m!"
             else:
                 print "\033[1;31m Create Faild! Please Check Config  \033[0m"
         except Exception as e:
             print e
 #print "\033[1;33m Will Resrart Reload\033[0m"
-print
 print "\033[1;32m  Will Resrart Reload\033[0m"
 os.system('/application/java/bin/java -jar /data/deploy/jenkins-cli.jar   -s http://192.168.1.62:7000 -auth %s:%s restart'%(jenkins_user,jenkins_passwd))

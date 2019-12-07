5f3f4a15900734345dfacc9e1c935f44f8d5ce6b
wangcq <wangcq@abb1513.com>
2019-12-07 15:10:17 +0800

    update prometheus配置，告警规则

--- python/alertmanager-Hook/app.py
+++ python/alertmanager-Hook/app.py
@@ -10,8 +10,18 @@
 from flask import Flask
 from flask import request,jsonify
 from dingding import *
+import time
 
 import logging
+
+
+
+url = {
+    "ops": "https://oapi.dingtalk.com/robot/send?access_token=ad336b5e9233b966526c63b8f17a946133a5727b6f92b0e32a397373fba50cce",
+    "ft":  "https://oapi.dingtalk.com/robot/send?access_token=87643be14f74b59a4247699c45a781a1352ea77aabab50c531db336c49b5503b",
+    "mt":  "https://oapi.dingtalk.com/robot/send?access_token=e72e440b8fe657492b2084d03f1ed81ae621f73c35ff88cc6259aeb0587d03b4"
+}
+
 
 
 app = Flask(__name__)
@@ -19,35 +29,44 @@ handler = logging.FileHandler('flask.log', encoding='utf8')
 logging_format = logging.Formatter(
     '%(asctime)s - %(levelname)s - %(filename)s - %(funcName)s - %(lineno)s - %(message)s')
 handler.setFormatter(logging_format)
-app.logger.info("收到回调")
 
 @app.route("/", methods=["post"])
 def send():
     data = request.json
-    alertname = data['groupLabels']['alertname']
-    status = data['status']
     alerts = data['alerts']
     send = Send_dingding()
     app.logger.info("收到回调")
     try:
         for i in alerts:
+            alertname = i["labels"]["alertname"]
+            status = i["status"]
             host = i['labels']['instance']
-            msg = i['annotations']['description']
-            test = """### Ops 网络报警 {type} \n #### status: *{status}* \n host: {host}  \n message: {msg}""".format(status=status, host=host, msg=msg, type=alertname)
-            if send.time_is():
-                res = send.send_to_dingding(alertname, test)
-                print(res['errcode'])
-                if res['errcode'] == 0:
-                    app.logger.info("钉钉发送成功, 发送消息: %s"%test)
-                else:
-                    app.logger.erroe("钉钉发送失败, 返回值 ====》%s"%res)
+            description = i['annotations']['description']
+            summary = i["annotations"]["summary"]
+            startTime = i["startsAt"]
+            service_name = i["labels"].get("service_name")
+            department = i["labels"].get("department")
+            # if status == "resolved":
+            #     endTime =  time.strptime(i["endsAt"],"%Y-%m-%d %H:%M:%S")
+            if service_name:
+                test = """### 测试监控报警: {summary} \n - 状态: **{status}** \n - 服务名称: {service_name} \n - 主机(端口): {host}\n  - 开始时间:{startTime} \n - 详细信息 : {msg}\n  """.format(
+                                    status=status, host=host, msg=description, type=alertname, summary = summary, startTime = startTime, service_name = service_name
+                        )
+            else:
+                test = """### 测试监控报警: {summary} \n - 状态: **{status}** \n - 主机(端口): {host}\n  - 开始时间: {startTime} \n - 详细信息\n : {msg}  """.format(
+                    status=status, host=host, msg=description, type=alertname, summary=summary, startTime=startTime
+                )
+            res = send.send_to_dingding(alertname, test, url["ops"]) # 默认发送到 ops
+            if department: # 判断部门 发往不同
+                res = send.send_to_dingding(alertname, test, url.get(department))
+            if res['errcode'] == 0: # 判断是否发送成功。
+                app.logger.info("钉钉发送成功, 发送消息: %s"%test)
+            else:
+                app.logger.erroe("钉钉发送失败, 返回值 ====》%s"%res)
     except Exception as e:
         app.logger.error(e)
-    return jsonify({'status': 200,"data":data})
-
-
     return jsonify({'status':200})
 if __name__ == '__main__':
     app.logger.setLevel(logging.INFO)
     app.logger.addHandler(handler)
-    app.run(host="0.0.0.0", port=6666, debug=True)
+    app.run(host="0.0.0.0", port=6666, debug=False)
--- python/alertmanager-Hook/dingding.py
+++ python/alertmanager-Hook/dingding.py
@@ -7,18 +7,19 @@
 
 import requests,json,datetime
 
+
 class Send_dingding:
-    def send_to_dingding(self,title, text):
+    def send_to_dingding(self,title, text, url):
         """
         :param title:  消息标题
         :param text:  消息内容
         :param url:   钉钉机器人url
         :return:  发送状态
         """
-        url = ''
+
         msg_text = {"msgtype": "markdown",
                     "markdown": {
-                        "title": "Ops 网络报警" + title ,
+                        "title": "监控报警" + title ,
                         "text": text
                         }
                     }
@@ -28,15 +29,3 @@ class Send_dingding:
         }
         res = requests.post(url, msg_text, headers=head)
         return json.loads(res.text)
-
-    def time_is(self):
-        d_time1 = datetime.datetime.strptime(str(datetime.datetime.now().date()) + '9:30', '%Y-%m-%d%H:%M')
-        d_time2 = datetime.datetime.strptime(str(datetime.datetime.now().date()) + '21:00', '%Y-%m-%d%H:%M')
-        now_time = datetime.datetime.now()
-        if  d_time1 < now_time < d_time2:
-            return True
-        else:
-            return False
-
-
-
